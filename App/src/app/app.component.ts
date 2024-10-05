547b31089929b524a019c16c63b33c13fa64b3f8
dmitriymaksimenko88@gmail.com <dmitriymaksimenko88@gmail.com>
2024-10-05 20:05:43 +0300

    feat: display data on the home page

--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataService } from './services/data.service';
+import { Professor } from './models/professor';
+import { Classroom } from './models/classroom';
+import { Course } from './models/course';
+import { Lesson } from './models/lesson';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +14,16 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'App';
+  classrooms: Classroom[];
+  courses: Course[];
+  lessons: Lesson[];
+  professors: Professor[];
+
+
+  constructor(private dataService: DataService) {
+    this.classrooms = this.dataService.getClassrooms();
+    this.courses = this.dataService.getCourses();
+    this.lessons = this.dataService.getSchedule();
+    this.professors = this.dataService.getProfessors();
+  }
 }
